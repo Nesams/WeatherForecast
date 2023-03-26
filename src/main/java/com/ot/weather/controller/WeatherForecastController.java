f7c34f4fb9b3d8fd1b887814bfef2365f237acdc
Nele Liis Sams <nesams@ttu.ee>
2023-03-26 11:43:03 +0300

    adding some tests

--- src/main/java/com/ot/weather/controller/WeatherForecastController.java
+++ src/main/java/com/ot/weather/controller/WeatherForecastController.java
@@ -4,7 +4,6 @@ package com.ot.weather.controller;
 import com.ot.weather.entity.GeneralForecast;
 import com.ot.weather.service.WeatherForecastService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
--- src/test/java/com/ot/weather/WeatherApplicationTests.java
+++ src/test/java/com/ot/weather/WeatherApplicationTests.java
@@ -1,7 +1,20 @@
 package com.ot.weather;
 
+import com.ot.weather.entity.GeneralForecast;
+import com.ot.weather.entity.Place;
+import com.ot.weather.entity.Wind;
+import com.ot.weather.repository.PlacesDataRepository;
+import com.ot.weather.repository.WeatherDataRepository;
+import com.ot.weather.repository.WindDataRepository;
+import com.ot.weather.service.TemperatureAverageService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.*;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class WeatherApplicationTests {
@@ -10,4 +23,54 @@ class WeatherApplicationTests {
 	void contextLoads() {
 	}
 
+	@Autowired
+	private PlacesDataRepository placesDataRepository;
+
+	@Autowired
+	private TemperatureAverageService temperatureAverageService;
+	@Autowired
+	private WeatherDataRepository weatherDataRepository;
+
+	@Autowired
+	private WindDataRepository windDataRepository;
+
+	@Test
+	public void testGetAverageWithCurrentData() throws IOException {
+		temperatureAverageService.getAverageTemperature();
+
+		double expectedMinTemp = 2.5;
+		double expectedMaxTemp = 4.0;
+
+		File file = new File("src/main/java/com/ot/weather/stats/temperature_stats.txt");
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+		assertEquals("Average minimum temperature: " + expectedMinTemp, reader.readLine());
+		assertEquals("Average maximum temperature: " + expectedMaxTemp, reader.readLine());
+		reader.close();
+
+	}
+
+	@Test
+	public void testAlwaysHaveFourGeneralForecasts() {
+		List<GeneralForecast> places = (List<GeneralForecast>) weatherDataRepository.findAll();
+		assertEquals(4, places.size());
+	}
+
+	@Test
+	public void testAlwaysHaveTwelvePlaces() {
+		List<Place> places = (List<Place>) placesDataRepository.findAll();
+		assertEquals(12, places.size());
+	}
+
+	@Test
+	public void testAlwaysHaveSixPlacesForNight() {
+		List<Place> places = placesDataRepository.findByNightOrDay("night");
+		assertEquals(6, places.size());
+	}
+
+	@Test
+	public void testAlwaysHaveSixWindSpeeds() {
+		List<Wind> windSpeeds = (List<Wind>) windDataRepository.findAll();
+		assertEquals(6, windSpeeds.size());
+	}
 }
+
